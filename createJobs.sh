23dd80c4aa1d3b596fa00dc4475dcc9aa6ab1ea0
devadathpt2 <swaroopayatham3@gmail.com>
2020-05-27 11:30:57 +0100

    adde new UI module

--- createJobs.sh
+++ createJobs.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for file in /job-definitions
 do
 
-jenkins-jobs --conf /etc/jenkins_jobs/jenkins_jobs.ini update "$file"
+sudo jenkins-jobs --conf /etc/jenkins_jobs/jenkins_jobs.ini update "$file"
 
 done
 
