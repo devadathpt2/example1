f4811d76d134de036b4dbd65cfcbbb524b138a53
devadathpt2 <swaroopayatham3@gmail.com>
2020-10-29 00:11:25 +0000

    added global Shared libary

--- createJobs.sh
+++ createJobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in /job-definitions
+for file in ./job-definitions
 do
 
 sudo /usr/local/Cellar/jenkins-job-builder/3.5.0/bin/jenkins-jobs --conf /etc/jenkins_jobs/jenkins_jobs.ini update "$file"
