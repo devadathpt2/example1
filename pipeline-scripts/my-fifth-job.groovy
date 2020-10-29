fbbd61addd9266b8200863d88f038ce7e57c0973
devadathpt2 <swaroopayatham3@gmail.com>
2020-10-29 08:56:06 +0000

    added global Shared libary

---
+++ pipeline-scripts/my-fifth-job.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+library 'Global-Common-libs'
+
+
+node()
+{
+
+        stage('runGroovy') {
+
+            // log.info 'Starting'
+
+            script {
+                helloSandhya()
+            }
+
+    }
+}
